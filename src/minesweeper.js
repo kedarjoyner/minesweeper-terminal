8e616c36656018d9b49c9e39e4e0a40acd597c1d
kedarjoyner <kedarjoyner@gmail.com>
2018-06-26 20:16:25 -0500

    Output and structure player boards

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,3 +1,6 @@
+
+// Dynamically generate player board
+
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
 
     let board = [];
@@ -15,3 +18,61 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
     
     return board;
 };
+
+// Dynamically generate bomb board
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs ) => {
+    let board = [];
+
+    for (let rowCounter = 0; rowCounter < numberOfRows; rowCounter++) {
+
+        let row = [];
+
+        for(let columnCounter = 0; columnCounter < numberOfColumns; columnCounter++ ) {
+
+            row.push(null);
+        }
+        board.push(row);
+        }
+
+    let numberOfBombsPlaced = 0;
+
+    while( numberOfBombsPlaced < numberOfBombs ) {
+
+        // Generate a random row index
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+
+        // Generate a random column index
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+        // Place bomb at random column and row indexes
+        board[randomRowIndex][randomColumnIndex] = 'B';
+
+        // Increment numberOfBombsPlace
+        numberOfBombsPlaced++;
+
+        // Add control flow to eliminate placing bombs over existing bombs
+    }
+  
+    return board;
+};
+
+
+
+// iterate through each board row
+// join individual board elements with line separator
+// join all rows together with enter separator
+const printBoard = board => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board: ');
+
+printBoard(playerBoard);
+
+console.log('Bomb Board: ');
+
+printBoard(bombBoard);
